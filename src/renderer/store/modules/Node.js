98ccae1207980afc659721cc2f53985d02a934d7
shunjinchan <shunjinchan@gmail.com>
2018-05-28 15:30:01 +0800

    update

---
+++ src/renderer/store/modules/Node.js
@@ -0,0 +1,11 @@
+import state from './Node/state'
+import getters from './Node/getters'
+import actions from './Node/actions'
+import mutations from './Node/mutations'
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
